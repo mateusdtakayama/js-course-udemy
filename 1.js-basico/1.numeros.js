a721e999e5de434d26381486831ffa3555041e80
Mateus Delai <mateus.takayama@ssys.com.br>
2023-03-08 16:54:28 -0300

    js-basico

---
+++ 1.js-basico/1.numeros.js
@@ -0,0 +1,14 @@
+let num1 = 15.123132131
+let num2 = 1231231
+let num3 = 12
+let num4 = 5
+
+console.log(num3.toString() + num4)
+
+num3 = num3.toString()
+
+console.log(typeof num3)
+
+console.log(num1.toFixed(2))
+
+console.log(Number.isInteger(num4)) // é inteiro?
---
+++ 1.js-basico/3.math.js
@@ -0,0 +1,11 @@
+let num = 1.123132;
+let num2  = Math.floor(num) // arredonda para baixo
+console.log(num2)
+
+let num3 = Math.ceil(num) // arredonda para cima
+console.log(num3)
+
+console.log(Math.round(num)) // arredonda para o mais proximo
+
+console.log(Math.max(1,2,3,4,5,6,7,10,123123,1,2,3) + ' ' + 'maior')
+console.log(Math.min(1,2,3,4,5,6,7,10,123123,1,2,3) + ' ' + 'menor')
